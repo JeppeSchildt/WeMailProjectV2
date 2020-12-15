3329a276361739d1e4f8e6a0e8efea917d5489b5
Gitgutgait <60431155+Gitgutgait@users.noreply.github.com>
2020-12-15 12:47:46 +0100

    Add files via upload

---
+++ Email/Email/Inbox.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace Email
+{
+    public partial class Inbox : Window
+    {
+        public Inbox()
+        {
+            InitializeComponent();
+        }
+
+        public static List<string> FindSent()               // function to fund the mails under sent folder and store in the list
+        {
+            string path = "S:/Email/Email/123/sent";
+            string[] extensions = { ".txt" };
+
+            var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories)
+                .Where(s => extensions.Any(ext => ext == System.IO.Path.GetExtension(s)));
+
+            List<string> textfile = new List<string>();
+
+            textfile = files.ToList();     // textfile er en list med string
+            return textfile;
+        }
+
+        private void Text()       // function to print the list of mails in sent folder
+        {
+            var numberOfLabels = 3;
+            List<string> FillIn = new List<string>();
+            FillIn = FindSent();      // FillIn er også en list med string
+
+            List<string> FileName = new List<string>();
+
+            FileName = FillIn;
+
+            for (int i = 1; i <= numberOfLabels; i++) {
+                FileName[i-1] = System.IO.Path.GetFileNameWithoutExtension(FillIn[i-1]);
+                var labelName = string.Format("Label{0}", i);
+                var label = (Label)this.FindName(labelName);
+                label.Content = FileName[i-1];
+            }
+        }
+
+        private void SentFolder_Click(object sender, RoutedEventArgs e)
+        {
+            Text();
+
+            TXT.hey();
+            TXT.read();
+        }
+    }
+}
---
+++ Email/Email/TXT.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Email
+{
+    public class TXT
+    {
+        // public string emailType, senderAddress, receiverAddress, timeStamp, subjectMatter, newContentText, oldContentText, emailFlag; //emailFlag can be read, unread, important
+
+
+        public static string emailType = "1",
+        senderAddress = "2f",
+            receiverAddress = "3",
+            timeStamp = "4",
+            subjectMatter = "BIL",
+            newContentText = "5",
+            oldContentText = "6",
+            emailFlag = "7";
+
+        public static void you(string inputEmailType, string inputSenderAddress, string inputReceiverAddress,
+           string inputTimeStamp, string inputSubjectMatter, string inputNewContentText, string inputOldContentText, string inputFlag)
+        {
+            emailType = inputEmailType;
+            senderAddress = inputSenderAddress;
+            receiverAddress = inputReceiverAddress;
+            timeStamp = inputTimeStamp;
+            subjectMatter = inputSubjectMatter;
+            newContentText = inputNewContentText;
+            oldContentText = inputOldContentText;
+            emailFlag = inputFlag;
+            Files();
+        }
+
+        public static void hey()
+        {
+            you(emailType, senderAddress, receiverAddress, timeStamp, subjectMatter, newContentText, oldContentText, emailFlag);
+
+        }
+        
+      
+        public static void Files ()
+        {
+            StreamWriter sw = new StreamWriter("S:/Email/Email/TranslateToTXT/" + subjectMatter + ".txt", true);
+            sw.WriteLine(emailType + "," + senderAddress + "," + receiverAddress + "," +timeStamp + "," + newContentText + ","
+                + oldContentText + "," + emailFlag );
+            sw.Flush();
+            sw.Close();
+        }
+
+        public static void read()
+        {
+            using (var sr = new StreamReader("S:/Email/Email/TranslateToTXT/" + subjectMatter+ ".txt"))  // read the directry of the userid and password
+           {
+
+
+                while (!sr.EndOfStream) {
+                    var line = sr.ReadLine();
+                    string[] words = line.Split(',');
+                    if (String.IsNullOrEmpty(line)) continue;
+                  
+
+                    Console.WriteLine(words[1]);
+
+                    Console.WriteLine(words[0]);
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
+
